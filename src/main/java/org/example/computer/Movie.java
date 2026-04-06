9d2f181e60af1d5c78fc5221de36c5c9a24386c7
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2026-04-06 14:28:36 +0530

    add movie model class

---
+++ src/main/java/org/example/computer/Movie.java
@@ -0,0 +1,18 @@
+package org.example.computer;
+
+
+public class Movie {
+    private String id;
+    private String name;
+    private String language;
+
+    public Movie(String id, String name, String language) {
+        this.id = id;
+        this.name = name;
+        this.language = language;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public String getLanguage() { return language; }
+}
